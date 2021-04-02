da4754b62d6d9e30cf99b500ac37fb908e0f8929
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-04-02 20:01:30 +0200

    Don't hardcoded gdal version on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,11 @@
-:: There is no `gdal-config` on Windows so we need to hardcode gdal's version.
-set GDAL_VERSION=3.2.2
+:: There is no `gdal-config` on Windows so we need figure it out from gdalinfo
+for /F "USEBACKQ tokens=2 delims=, " %%F in (`gdalinfo --version`) do (
+set GDAL_VERSION=%%F
+)
+if errorlevel 1 exit 1
+echo "set GDAL_VERSION=%GDAL_VERSION%"
 
-"%PYTHON%" -m pip install --no-use-pep517 -vv --no-deps --ignore-installed . ^
+"%PYTHON%" -m pip install --no-use-pep517 -vv . ^
                           --global-option=build_ext ^
                           --global-option="-I%LIBRARY_INC%" ^
                           --global-option="-L%LIBRARY_LIB%" ^
