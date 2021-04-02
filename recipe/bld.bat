1d65115aeba74b56239640a43dec7597ccea0ab5
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-04-02 08:52:47 +0200

    add build scripts

---
+++ recipe/bld.bat
@@ -0,0 +1,9 @@
+:: There is no `gdal-config` on Windows so we need to hardcode gdal's version.
+set GDAL_VERSION=3.1.4
+
+"%PYTHON%" -m pip install --no-deps --ignore-installed . ^
+                          --global-option=build_ext ^
+                          --global-option="-I%LIBRARY_INC%" ^
+                          --global-option="-L%LIBRARY_LIB%" ^
+                          --global-option="-lgdal_i"
+if errorlevel 1 exit 1
