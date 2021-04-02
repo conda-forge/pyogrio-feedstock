a01e99e8575f27e58f51f8fc8f058d203774a1db
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-04-02 09:45:51 +0200

    try no pep517 + verbose

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 :: There is no `gdal-config` on Windows so we need to hardcode gdal's version.
 set GDAL_VERSION=3.2.2
 
-"%PYTHON%" -m pip install --no-deps --ignore-installed . ^
+"%PYTHON%" -m pip install --no-use-pep517 -vv --no-deps --ignore-installed . ^
                           --global-option=build_ext ^
                           --global-option="-I%LIBRARY_INC%" ^
                           --global-option="-L%LIBRARY_LIB%" ^
