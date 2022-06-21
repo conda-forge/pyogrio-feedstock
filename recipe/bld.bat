19c9f8fa0f260ff48ac1df378f2b119639c17cd1
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-06-21 18:01:33 +0200

    pyogrio v0.4.0 (#26)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,9 +5,8 @@ set GDAL_VERSION=%%F
 if errorlevel 1 exit 1
 echo "set GDAL_VERSION=%GDAL_VERSION%"
 
-"%PYTHON%" -m pip install --no-use-pep517 -vv . ^
-                          --global-option=build_ext ^
-                          --global-option="-I%LIBRARY_INC%" ^
-                          --global-option="-L%LIBRARY_LIB%" ^
-                          --global-option="-lgdal_i"
+set GDAL_INCLUDE_PATH=%LIBRARY_INC%
+set GDAL_LIBRARY_PATH=%LIBRARY_LIB%
+
+%PYTHON% -m pip install --no-deps --ignore-installed -v .
 if errorlevel 1 exit 1
